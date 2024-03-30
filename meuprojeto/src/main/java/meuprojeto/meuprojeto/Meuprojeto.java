a33b5fa611a7758dcf51e3ab5536284c2d176349
FedzerDesruisseaux <marieyolene099@gmail.com>
2024-03-30 16:25:44 -0300

    Primeiro commit

---
+++ meuprojeto/src/main/java/meuprojeto/meuprojeto/Meuprojeto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package meuprojeto.meuprojeto;
+
+/**
+ *
+ * @author User
+ */
+public class Meuprojeto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
